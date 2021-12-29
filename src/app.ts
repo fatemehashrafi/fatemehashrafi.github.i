f2dd197aa58119af67a932969e3434436e74ed16
fatemeh <you@example.com>
2021-12-29 18:27:10 +0330

    added app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,18 @@
 import debug from "debug";
+const generallog:debug.Debugger=debug("app:general");
 
-class hero {
+class hero{
     name:string
     _health:number
     xp:number
     hunger:number
+    debug :Debugger
     constructor() {
         this.name = "";
         this._health = 100;
         this.xp = 2;
         this.hunger = 6;
-        const handler = setInterval(() => {
-            this.hunger--;
-            this.log('man mordam', this.hunger);
-            if (this.amIDead())
-            this.log("i am dead");
-            clearInterval(handler);
-        }, 4000);
+        this.log=debug('man mordam ${this.hunger}');     
     }
     set health(input) {
         this._health = input
@@ -25,6 +21,7 @@ class hero {
         return this.health;
     }
     attack() {
+        this.hunger--;
         this.log("attacking");
     }
     amIDead() {
@@ -32,64 +29,64 @@ class hero {
     }
 }
 
-class archer extends hero {
-    attack() {
-        this.log("archer attacking");
-    }
+// class archer extends hero {
+//     attack() {
+//         this.log("archer attacking");
+//     }
 
-}
-class knight extends hero {
-    sword:number
-    constructor(name, sword) {
-        super(name);
-        this.sword = 6;
-    }
-}
-class soldier extends hero{
-    name:string
-    knight:string
-    constructor(name,knight){
-        this.name=name;
-        this.knight=knight;
-    }
-}
+// }
+// class knight extends hero {
+//     sword:number
+//     constructor(name, sword) {
+//         super(name);
+//         this.sword = 6;
+//     }
+// }
+// class soldier extends hero{
+//     name:string
+//     knight:string
+//     constructor(name,knight){
+//         this.name=name;
+//         this.knight=knight;
+//     }
+// }
 
-class clan{
-    name : string = "";
-    army : hero[]=[]
-    constructor( name: string ,army : hero[]){
-        this.army=army;
-        this.name=name;
-    }
-   attack(enemy:number){
-       console.log("i am attacking")
-       soldier1.attack(enemy.army[Math.floor(Math.random() * this.army.length)]);
-       enemy.forEach(hero => {
-           hero.attack()
-       });
-   }
-}
+// class clan{
+//     name : string = "";
+//     army : hero[]=[]
+//     constructor( name: string ,army : hero[]){
+//         this.army=army;
+//         this.name=name;
+//     }
+//    attack(enemy:number){
+//        console.log("i am attacking")
+//        soldier1.attack(enemy.army[Math.floor(Math.random() * this.army.length)]);
+//        enemy.forEach(hero => {
+//            hero.attack()
+//        });
+//    }
+// }
 
-const clan =new clan()
-clan1.army.push(new soldier ,new knight)
-const knight1=new knight();
-const knight2=new knight();
-const knight3=new knight();
-const knight4=new knight();
-const soldier1=new soldier();
-const soldier2=new soldier();
-const soldier3=new soldier();
-const soldier4=new soldier();
-const soldier5=new soldier();
-const soldier6=new soldier();
-soldier1.attack(knight1 )
+// const clan =new clan()
+// clan1.army.push(new soldier ,new knight)
+// const knight1=new knight();
+// const knight2=new knight();
+// const knight3=new knight();
+// const knight4=new knight();
+// const soldier1=new soldier();
+// const soldier2=new soldier();
+// const soldier3=new soldier();
+// const soldier4=new soldier();
+// const soldier5=new soldier();
+// const soldier6=new soldier();
+// soldier1.attack(knight1 )
 
 
 
 async function logtest() {
     let i:number=0
     for(i=0 ;i<10 ;i++){
-        generallog('index is' ${i})
+        generallog('index is ${i}');
     }
 }
 logtest()
